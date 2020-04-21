6a427f625cb7c9a50f0c80abeabb39c4479e5980
Phill Sparks <me@phills.me.uk>
2020-04-21 14:39:55 +0100

    Run elm-format and turn results into warnings

---
+++ __tests__/elm/Bad.elm
@@ -0,0 +1,5 @@
+module Bad exposing (main)
+
+--- This module is intentionally formatted badly
+main : String
+main = "Hello World"
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,69 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import {issueCommand} from '@actions/core/lib/command'
+
+const elmFormatCmd = core.getInput('elm_format', {
+  required: true
+})
+const elmFiles = core.getInput('elm_files', {
+  required: true
+})
+
+const runElmFormat = async (): Promise<Report> => {
+  let output = ''
+  let errput = ''
+
+  const options = {
+    ignoreReturnCode: true,
+    listeners: {
+      stdout: (data: Buffer) => {
+        output += data.toString()
+      },
+      stderr: (data: Buffer) => {
+        errput += data.toString()
+      }
+    },
+    silent: true
+  }
+
+  await exec.exec(elmFormatCmd, [elmFiles, '--validate'], options)
+
+  if (errput.length > 0) {
+    throw Error(errput)
+  }
+
+  return JSON.parse(output)
+}
+
+type Report = Message[]
+
+interface Message {
+  path: string
+  message: string
+}
+
+const issueErrors = (report: Report): number => {
+  let reported = 0
+  for (const message of report) {
+    issueCommand('error', {file: message.path}, message.message)
+    reported++
+  }
+  return reported
+}
+
+const reportFailure = (reported: number): void => {
+  if (reported) {
+    core.setFailed(
+      `elm-format reported errors with ${reported} ${
+        reported === 1 ? 'file' : 'files'
+      }`
+    )
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    await runElmFormat().then(issueErrors).then(reportFailure)
   } catch (error) {
     core.setFailed(error.message)
   }
